95f288578a2817d875aaa7d2d5b9d3283ad1957a
Nayar Joolfoo <nayar@joolfoo.com>
2018-11-19 09:31:16 +0000

    delete contact and user cascade for sip

--- ipbxmanager/ipbxmanager/doctype/sip_user/sip_user.py
+++ ipbxmanager/ipbxmanager/doctype/sip_user/sip_user.py
@@ -5,6 +5,29 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+import json
+from pprint import pprint
 
 class SIPUser(Document):
-	pass
+    def validate(self):
+        company = frappe.get_doc("Company",self.company)
+        self.name = self.sip_user_id + '@' + company.domain
+
+    def on_update(self):
+        company = frappe.get_doc("Company",self.company)
+        doc = frappe.get_doc({
+            "doctype": "User",
+            "email" : self.name,
+            "first_name" : self.sip_user_id,
+            "last_name" : company.domain
+        })
+        doc.insert()
+
+    def on_trash(self):
+        user = frappe.get_doc("User",self.name)
+        contact = frappe.get_doc("Contact",user.full_name)
+        contact.delete()
+        user.delete()
+
+
+
