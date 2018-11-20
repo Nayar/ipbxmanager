a6f1e8af498143a10920dc69105b86760ec021e2
Nayar Joolfoo <nayar@joolfoo.com>
2018-11-20 10:06:26 +0000

    make work

--- ipbxmanager/ipbxmanager/web_form/sip_accounts/sip_accounts.js
+++ ipbxmanager/ipbxmanager/web_form/sip_accounts/sip_accounts.js
@@ -2,7 +2,7 @@ frappe.ready(function() {
 	// bind events here
     //frappe.msgprint('Please fill all values carefully');
     console.log(frappe.web_form)
-    frappe.web_form.set_value('customer', 'Raj Moloo');
+    frappe.web_form.set_value('sip_domain', 'Raj Moloo');
     
 })
 
