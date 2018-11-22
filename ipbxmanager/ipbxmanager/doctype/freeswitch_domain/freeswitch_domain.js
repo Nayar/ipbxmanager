f564373de14dcd60c8433768cd22b12aad6e98bc
Nayar Joolfoo <nayar@joolfoo.com>
2018-11-22 10:55:28 +0000

    autogenerate users

--- ipbxmanager/ipbxmanager/doctype/freeswitch_domain/freeswitch_domain.js
+++ ipbxmanager/ipbxmanager/doctype/freeswitch_domain/freeswitch_domain.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2018, Nayar Joolfoo and contributors
 // For license information, please see license.txt
-
+var i = 1;
 frappe.ui.form.on('Freeswitch Domain', {
 	refresh: function(frm) {
+        frm.add_custom_button(__("Generate Users"), function(){
+            console.log(frappe)
+            console.log(frm)
 
+            var newrow = frappe.model.add_child(frm.doc, "SIP User", "sip_users");
+            newrow.sip_user_id = String(100 + i++)
+
+            frm.refresh()
+        });
 	}
 });
