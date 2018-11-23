fadb3cf354a7431fc837479134137fb96afda9f0
Nayar Joolfoo <nayar@joolfoo.com>
2018-11-23 05:59:59 +0000

    do not generate duplicates

--- ipbxmanager/ipbxmanager/doctype/freeswitch_domain/freeswitch_domain.js
+++ ipbxmanager/ipbxmanager/doctype/freeswitch_domain/freeswitch_domain.js
@@ -1,5 +1,15 @@
 // Copyright (c) 2018, Nayar Joolfoo and contributors
 // For license information, please see license.txt
+
+function check_sip_exists(sip_users,sip_user_id){
+    for(var i =0 ; i < sip_users.length ; i++) {
+        if(sip_users[i].sip_user_id == sip_user_id) {
+            return true;
+        }
+    }
+    return false;
+}
+
 var i = 1;
 frappe.ui.form.on('Freeswitch Domain', {
 	refresh: function(frm) {
@@ -12,9 +22,17 @@ frappe.ui.form.on('Freeswitch Domain', {
                 function(values){
                     show_alert(values, 5);
                     console.log(values)
-                    for(var i = 0; i < values.no_of_users_to_generate ; i++){
-                        var newrow = frappe.model.add_child(frm.doc, "SIP User", "sip_users");
-                        newrow.sip_user_id = String(1000 + i)
+                    var i = 0;
+                    while (i < values.no_of_users_to_generate) {
+                        var new_sip_user_id = 1000 + i
+                        if(check_sip_exists(frm.doc.sip_users,new_sip_user_id) == false){
+                            var newrow = frappe.model.add_child(frm.doc, "SIP User", "sip_users");
+                            newrow.sip_user_id = String(1000 + i)
+                        }
+                        else {
+                            values.no_of_users_to_generate++
+                        }
+                        i++
                     }
                     frm.refresh()
                 },
