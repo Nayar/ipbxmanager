e2ebbe6b9d0b87c7ecc331511b3fbf72226b83df
Nayar Joolfoo <nayar@joolfoo.com>
2018-11-23 05:39:58 +0000

    generate users

--- ipbxmanager/ipbxmanager/doctype/freeswitch_domain/freeswitch_domain.js
+++ ipbxmanager/ipbxmanager/doctype/freeswitch_domain/freeswitch_domain.js
@@ -6,11 +6,21 @@ frappe.ui.form.on('Freeswitch Domain', {
         frm.add_custom_button(__("Generate Users"), function(){
             console.log(frappe)
             console.log(frm)
-
-            var newrow = frappe.model.add_child(frm.doc, "SIP User", "sip_users");
-            newrow.sip_user_id = String(100 + i++)
-
-            frm.refresh()
+            frappe.prompt([
+                    {'fieldname': 'no_of_users_to_generate', 'fieldtype': 'Int', 'label': 'No of users to generate', 'reqd': 1}  
+                ],
+                function(values){
+                    show_alert(values, 5);
+                    console.log(values)
+                    for(var i = 0; i < values.no_of_users_to_generate ; i++){
+                        var newrow = frappe.model.add_child(frm.doc, "SIP User", "sip_users");
+                        newrow.sip_user_id = String(1000 + i)
+                    }
+                    frm.refresh()
+                },
+                '',
+                'Generate Users'
+            )
         });
 	}
 });
