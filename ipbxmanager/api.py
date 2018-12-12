743f1502efff86036b4d51d3e19d4020c7088332
Nayar Joolfoo <nayar@joolfoo.com>
2018-12-12 09:06:44 +0000

    update

--- ipbxmanager/api.py
+++ ipbxmanager/api.py
@@ -4,14 +4,17 @@ import frappe
 def domain_valid(domain):
 	return True
 
-@frappe.whitelist()
-def add_company(domain, company_name, custom_domain):
+@frappe.whitelist(allow_guest=True)
+def add_company(domain, company_name, custom_domain,company_brn,contact_name,contact_email):
 	if(custom_domain == 'no'):
 		domain = domain + '.pbx.joolfoo.com'
 	doc = frappe.get_doc({
 		"doctype": "Freeswitch Domain",
 		"company_name": company_name,
-		"sip_domain" : domain
+		"sip_domain" : domain,
+		'company_brn' : company_brn,
+		'contact_name' : contact_name,
+		'contact_email': contact_email
 	})
 	doc.insert()
 	return True
