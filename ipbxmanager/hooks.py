3c70bf4879852b16ac14b23e0ebc0dac969c53a1
Nayar Joolfoo <nayar@joolfoo.com>
2018-12-17 13:39:59 +0000

    rename order page

--- ipbxmanager/hooks.py
+++ ipbxmanager/hooks.py
@@ -35,7 +35,7 @@ app_license = "MIT"
 # ----------
 
 # application home page (will override Website Settings)
-home_page = "placeorder"
+home_page = "orderipbx"
 
 # website user home page (by Role)
 # role_home_page = {
