8eb931017f9d5b1417142a5fb7ea7a3c0c6618a6
Nayar Joolfoo <nayar@joolfoo.com>
2018-12-18 05:29:10 +0000

    add company website and position

--- ipbxmanager/api.py
+++ ipbxmanager/api.py
@@ -5,7 +5,7 @@ def domain_valid(domain):
 	return True
 
 @frappe.whitelist(allow_guest=True)
-def add_company(domain, company_name, custom_domain,company_brn,contact_name,contact_email):
+def add_company(domain, company_name, custom_domain,company_brn,contact_name,contact_email,contact_position,company_website):
 	if(custom_domain == 'no'):
 		domain = domain + '.pbx.joolfoo.com'
 	doc = frappe.get_doc({
@@ -14,7 +14,9 @@ def add_company(domain, company_name, custom_domain,company_brn,contact_name,con
 		"sip_domain" : domain,
 		'company_brn' : company_brn,
 		'contact_name' : contact_name,
-		'contact_email': contact_email
+		'contact_email': contact_email,
+		'contact_position' : contact_position,
+		'company_website' : company_website
 	})
 	doc.insert()
 	return True
