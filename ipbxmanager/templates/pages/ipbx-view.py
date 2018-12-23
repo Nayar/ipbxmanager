da831299ebbb107b3e8b3b7ed0653532901a9546
Nayar Joolfoo <nayar@joolfoo.com>
2018-12-23 18:18:05 +0000

    remove extra pages

--- ipbxmanager/templates/pages/ipbx-view.py
+++
@@ -1,9 +0,0 @@
-import pprint
-import frappe
-
-def get_context(context):
-	print('here')
-	pprint.pprint(frappe)
-	pprint.pprint(context)
-	context['custom_content'] = 'Hub featured page custom content'
-	context.show_sidebarar = 1
--- ipbxmanager/templates/pages/orderipbx.py
+++
@@ -1,7 +0,0 @@
-import pprint
-import frappe
-
-def get_context(context):
-    pprint.pprint(frappe)
-    pprint.pprint(context)
-    context['custom_content'] = 'Hub featured page custom content'
