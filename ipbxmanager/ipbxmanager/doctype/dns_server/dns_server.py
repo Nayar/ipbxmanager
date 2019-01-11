b5c8ced0860272e418a6e2d5dd72b2b0421084a5
Nayar Joolfoo <nayar@joolfoo.com>
2019-01-11 10:15:20 +0000

    remove ssh deploys

--- ipbxmanager/ipbxmanager/doctype/dns_server/dns_server.py
+++ ipbxmanager/ipbxmanager/doctype/dns_server/dns_server.py
@@ -9,40 +9,4 @@ from frappe.model.document import Document
 
 
 class DNSServer(Document):
-	def ssh_command(self,cmd):
-		import base64
-		import paramiko
-		key = paramiko.RSAKey.from_private_key_file("/home/frappe/.ssh/id_rsa")
-		client = paramiko.SSHClient()
-		client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		client.connect(self.ip, username='root')
-		stdin, stdout, stderr = client.exec_command(cmd)
-		client.close()
-		return stdin, stdout, stderr
-	
-	def add_domain(self,domain,A):
-		zone_str = """zone "%s" { 
-type master; 
-file "/etc/bind/zones/%s.db"; 
-};""" % (domain,domain)
-
-		zone_data = """$TTL 5
-@ IN SOA ns1.joolfoo.com webmaster.joolfoo.com. (
-201006601 ; Serial
-5 ; Refresh
-120 ; Retry
-2419200 ; Expire
-5) ; Default TTL
-;
-
-
-%s. IN A %s
-@ IN NS ns1.joolfoo.com.""" % (domain,A)
-		stdin, stdout, stderr = self.ssh_command("echo '" + zone_str + "' > /etc/bind/named.conf.local")
-		stdin, stdout, stderr = self.ssh_command("echo '%s' > /etc/bind/zones/%s.db" % (zone_data,domain))
-		stdin, stdout, stderr = self.ssh_command("systemctl restart bind9 2>&1")
-		for line in stdout:
-			print('... ' + line.strip('\n'))
-		for line in stderr:
-			print('... ' + line.strip('\n'))
-		print('kkkkkkkk')
+  pass
